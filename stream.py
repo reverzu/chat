ac4e2f3a9fe655d4d3da5ac3d215f9d8077f3449
Abhijeet Khatri <76448535+reverzu@users.noreply.github.com>
2024-02-08 01:15:00 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -8,7 +8,8 @@ model = LLaMAForCausalLM.from_pretrained( "decapoda-research/llama-7b-hf", load_
 model = PeftModel.from_pretrained(model, "samwit/alpaca7B-lora")
 def alpaca_talk(text): inputs = tokenizer( text, return_tensors="pt", ) 
 input_ids = inputs["input_ids"].cuda() 
-generation_config = GenerationConfig( temperature=0.6, top_p=0.95, repetition_penalty=1.2, ) print("Generating...") 
+generation_config = GenerationConfig( temperature=0.6, top_p=0.95, repetition_penalty=1.2, ) 
+st.write("Generating...") 
 generation_output = model.generate( input_ids=input_ids, generation_config=generation_config, return_dict_in_generate=True, output_scores=True, max_new_tokens=256, ) 
 for s in generation_output.sequences: st.write(tokenizer.decode(s))
 
